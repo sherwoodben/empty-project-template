7bcc1d4d02f7ab884dfae0bfc828b68affe7ae05
sherwoodben <28694955+sherwoodben@users.noreply.github.com>
2025-10-10 03:10:12 -0500

    fix: moved the example project creation to a separate file (#5)

---
+++ examples/premake/premake5.lua
@@ -0,0 +1,4 @@
+-- Add PROJECTS to this file as needed!
+
+-- Or, if it makes more sense to store examples in subdirectories, maybe add
+-- a premake5.lua file to the subdirectory and include it here!
